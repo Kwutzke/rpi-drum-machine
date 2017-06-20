d6f6ad61405e1078e3aab372089a2c6f113f3f7d
Kwutzke <Kwkw@me.com>
2017-06-20 13:51:20 +0200

    cpp drum machine project created

---
+++ DrumMachine/cmake-build-debug/CMakeFiles/DrumMachine.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DrumMachine.dir/main.cpp.o"
+  "DrumMachine.pdb"
+  "DrumMachine"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DrumMachine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
