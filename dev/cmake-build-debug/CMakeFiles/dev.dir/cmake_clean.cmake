e920597402ed2da31de810f5f9030b8ca53a31f6
Kwutzke <Kwkw@me.com>
2017-06-27 20:24:14 +0200

    sdl2 example added

---
+++ dev/cmake-build-debug/CMakeFiles/dev.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dev.dir/main.cpp.o"
+  "dev.pdb"
+  "dev"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dev.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
