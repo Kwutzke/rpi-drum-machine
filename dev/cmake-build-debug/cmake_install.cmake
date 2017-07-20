04964a1713d0b1ecf70cc7e98a3dd4290ab6005a
Kwutzke <Kwkw@me.com>
2017-07-20 13:26:22 +0200

    revert

--- dev/cmake-build-debug/cmake_install.cmake
+++ dev/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/kilian/Development/EntwMM/Arduino Drum Machine/DrumMachine
+# Install script for directory: /Users/kilian/Development/EntwMM/Arduino Drum Machine/dev
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -35,5 +35,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/kilian/Development/EntwMM/Arduino Drum Machine/DrumMachine/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/kilian/Development/EntwMM/Arduino Drum Machine/dev/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
---
+++ untitled1/cmake-build-debug/CMakeFiles/untitled1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled1.dir/main.cpp.o"
+  "untitled1.pdb"
+  "untitled1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/untitled1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
